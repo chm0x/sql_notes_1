fd09325c1e7a8ad963b521da29fbb4a47ac808d7
Jose Maria <chchema26@gmail.com>
2024-03-16 22:51:30 -0600

    Rank and dense rank

--- 20-window-functions/05-rank-and-dense-rank.sql
+++ 20-window-functions/05-rank-and-dense-rank.sql
@@ -85,3 +85,82 @@ FROM
     salaries 
 WHERE emp_no = 11839
 WINDOW w AS (PARTITION BY emp_no ORDER BY salary DESC);
+
+
+----------------------------------------------------------
+/*
+    EXERCISE 1:
+    Write a query containing a window function to 
+    obtain all salary values that employee number
+    10560 has ever signed a contract for.
+
+    Order and display the obtained salary values
+    from highest to lowest.
+*/
+SELECT 
+    emp_no,
+    salary,
+    ROW_NUMBER() OVER w AS row_num
+FROM
+    salaries 
+WHERE emp_no = 10560 
+WINDOW w AS (PARTITION BY emp_no ORDER BY salary DESC)
+LIMIT 20;
+
+/*
+    EXERCISE 2:
+    Write a query that upon execution, displays the number 
+    of salary contracts that each manager has ever 
+    signed while working in the company.
+*/
+SELECT 
+    dm.emp_no,
+    (COUNT(s.salary)) AS no_of_salary_contracts
+FROM 
+    dept_manager dm
+JOIN
+    salaries s ON dm.emp_no = s.emp_no
+GROUP BY emp_no
+ORDER BY emp_no
+LIMIT 50;
+
+
+/*
+    EXERCISE 3:
+    Write a query that upon execution retrieves
+    a result set containing all salary values
+    that employee 10560 has ever signed a contract for. 
+    Use a window function to rank all salary values 
+    from highest to lowest in a way that equal salary 
+    values bear the same rank and that gaps in 
+    the obtained ranks for subsequent rows are allowed.
+*/
+SELECT 
+    emp_no,
+    salary,
+    RANK() OVER w AS row_rank
+FROM 
+    salaries 
+WHERE emp_no = 10560
+WINDOW w AS (PARTITION BY emp_no ORDER BY salary DESC)
+LIMIT 20;
+
+/*
+    EXERCISE 4:
+    Write a query that upon execution retrieves 
+    a result set containing all salary values
+    that employee 10560 has ever signed a contract for. 
+    Use a window function to rank all salary values 
+    from highest to lowest in a way that equal salary 
+    values bear the same rank and that gaps in 
+    the obtained ranks for subsequent rows are not allowed.
+*/
+SELECT 
+    emp_no,
+    salary,
+    DENSE_RANK() OVER w AS den_rank
+FROM 
+    salaries
+WHERE emp_no = 10560
+WINDOW w AS (PARTITION BY emp_no ORDER BY salary DESC)
+LIMIT 20;
